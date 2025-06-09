66fe6e3f87f4ca96f8c9442121a4b814b39d2970
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-06-09 15:42:55 +0530

    2:55

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,7 +4,10 @@ const footer = () => {
   return (
     <>
       <div className="footer flex justify-center items-center absolute bottom-6 left-1/2">
-        <p>Made with ❤️ by <span className='text-green-500 font-bold'><a href="https://itzabhinavarya.netlify.app">Abhinav Arya</a></span></p>
+        <p>Made with ❤️ by <span className='text-green-500 font-bold'>
+          {/* <a href="https://itzabhinavarya.netlify.app">Shekhar Rana</a> */}
+          <span>Shekhar Rana</span>
+          </span></p>
       </div>
     </>
   )
